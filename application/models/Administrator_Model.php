1cd0d73fa144d39f5e5e275fa97262febb0707ca
Tejaswi Dessai <Tejaswi.Dessai@mehp.com>
2021-03-02 18:01:02 +0530

    query echo removed, agian chanes to echo

--- application/models/Administrator_Model.php
+++ application/models/Administrator_Model.php
@@ -2118,10 +2118,11 @@ public function get_campaign_fordataverification()
 				$this->db->select("lms.id, lms.rejected, lms.accepted, (lms.accepted+lms.rejected) as numbers,(lms.accepted+lms.rejected) as numberveri, (lms.accepted+lms.rejected)-(lms.accepted+lms.rejected) as pending, users.fname, campaign.cids,campaign.campnm from
 			(select l1.cids, l1.id, l1.accepted, l2.rejected from
 			(select cids, dvagtidi as id, count(dvagtidi) as accepted  from leadmaster where dvagtidi is not null
+			AND  dvdti >= '$from' AND dvdti <= '$to'
 			group by dvagtidi, cids ) as l1
 			inner join 
 			(select cids, dvragtidi as ids, count(dvragtidi)as rejected from leadmaster where dvragtidi is not null
-			  AND  dvrdti >= '$from' AND dvrdti <= '$to' AND  dvdti >= '$from' AND dvdti <= '$to'  group by dvragtidi, cids) as l2
+			  AND  dvrdti >= '$from' AND dvrdti <= '$to'   group by dvragtidi, cids) as l2
 			on l1.id = l2.ids) as lms");
 			}else{
 				$this->db->select("lms.id, lms.rejected, lms.accepted, (lms.accepted+lms.rejected) as numbers,(lms.accepted+lms.rejected) as numberveri, (lms.accepted+lms.rejected)-(lms.accepted+lms.rejected) as pending, users.fname, campaign.cids,campaign.campnm from
@@ -2178,7 +2179,7 @@ public function get_campaign_fordataverification()
 			// $this->db->group_by('campaign.campnm');
 			
 			$query=$this->db->get();
-			// echo $this->db->last_query(); 
+			echo $this->db->last_query(); 
 			return $data=$query->result_array();
 
 		}	

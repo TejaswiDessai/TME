05fa87d05d803266f2f8187b2f0b93d5dac50e65
Amol Waghmare <Amol.Waghmare@mehp.com>
2021-03-10 18:27:33 +0530

    campaign report menu code updated

--- application/views/administrator/header-bottom.php
+++ application/views/administrator/header-bottom.php
@@ -166,7 +166,7 @@ $userteam = explode(',',$userteam1);
                         </a>
                         <ul class="tree-1">
                             <li><a href="<?php echo base_url(); ?>administrator/user_report" data-i18n="nav.basic-components.alert">User Report</a></li>
-                            <li><a href="<?php //echo base_url(); ?>campaign_report" data-i18n="nav.basic-components.breadcrumbs">Campaign Report</a></li>
+                            <li><a href="<?php echo base_url(); ?>administrator/campaign_report" data-i18n="nav.basic-components.breadcrumbs">Campaign Report</a></li>
                             <!-- <li><a href="<?php echo base_url(); ?>administrator/selectCampaignForEmailVerification" data-i18n="nav.basic-components.alert">Email Verification</a></li> -->
                         </ul>
                     </li>

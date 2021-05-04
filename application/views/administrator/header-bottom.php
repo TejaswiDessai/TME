764b5c1a4ac573f309ade2a7b90eecb7d93c5936
Amol Waghmare <Amol.Waghmare@mehp.com>
2021-05-04 16:40:54 +0530

    ev report and header text changes

--- application/views/administrator/header-bottom.php
+++ application/views/administrator/header-bottom.php
@@ -59,7 +59,7 @@ $userteam = explode(',',$userteam1);
                         <ul class="tree-1">
                             <li><a href="<?php echo base_url(); ?>campaigns/addcampaignForm">Add Campaign</a></li>
                              <li><a href="<?php echo base_url(); ?>campaigns/campaign">Campaign List</a></li>
-                             <li><a href="<?php echo base_url(); ?>administrator/selectCampaignforReuse">Campaign Reuse</a></li>
+                             <li><a href="<?php echo base_url(); ?>administrator/selectCampaignforReuse">Reuse Data</a></li>
                         </ul>
                     </li>
                 <?php } ?>

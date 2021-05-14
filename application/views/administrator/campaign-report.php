ef05e16604713f1f496da6ad70065dd3b146f2a9
Amol Waghmare <Amol.Waghmare@mehp.com>
2021-05-14 16:18:45 +0530

    campaign report in process count code updated

--- application/views/administrator/campaign-report.php
+++ application/views/administrator/campaign-report.php
@@ -304,13 +304,16 @@
                                         </td>
                                         <td>
                                         <?php 
-                                         $ev_inproccess = $this->db->query("select distinct(ev.lmid) from ev
-                                         left join leadmaster ON ev.lmid=leadmaster.lmid
-                                         where
-                                         leadmaster.evcomp = 2
-                                         and leadmaster.cids = '".$post['cids']."'
+                                        $ev_inproccess = $this->db->query("SELECT count(*) FROM 'ev' LEFT JOIN 'leadmaster' ON 'ev'.'lmid' = 'leadmaster'.'lmid' LEFT JOIN 'users' ON 'users'.'empcode' = 'leadmaster'.'stagtidi' 
+                                        LEFT JOIN 'campaign' ON 'campaign'.'cids' = 'leadmaster'.'cids' WHERE 'ev'.'evagnt' = '92103' AND 'leadmaster'.'cids' = '1001' AND 'ev'.'curr_active' = 1 AND 'ev'.'closer_status' = 'Open' 
+                                        ");
+                                        //  $ev_inproccess = $this->db->query("select distinct(ev.lmid) from ev
+                                        //  left join leadmaster ON ev.lmid=leadmaster.lmid
+                                        //  where
+                                        //  leadmaster.evcomp = 2
+                                        //  and leadmaster.cids = '".$post['cids']."'
                                          
-                                         ");
+                                        //  ");
                                         
                                          echo $ev_inproccess->num_rows();
                                          ?>
